e047886af704507d961cd71f6b2b437bbde46101
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-08-04 09:34:13 +0300

    update changes

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,20 +1,12 @@
-import React from "react";
+import React from 'react';
 
-// no need to modify anything in here. The final test is looking for the returned JSX from this component
 function TestComponent() {
   return (
-    <div className="video">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/5TbUxGZtwGI"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        title="time video"
-        allowFullScreen
-      ></iframe>
+    <div title="time video">
+      <p>This is a test component</p>
     </div>
   );
 }
 
 export default TestComponent;
+
